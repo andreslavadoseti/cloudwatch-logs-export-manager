bcfb344a473b3aa5b0cb1f1347d51cf5138a0e95
andreslavado <andreslavado@seti.com.co>
2021-01-13 14:33:46 -0500

    Remove file utils.js

--- utils.js
+++
@@ -1,7 +0,0 @@
-module.exports.variables = (serverless) => {
-    serverless.cli.consoleLog('Processing variables in utils.js');
-    serverless.cli.consoleLog(serverless.cli);
-    return {
-        retentionInDays: parseInt(serverless.provider.retentionInDays)
-    };
-};
